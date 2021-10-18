5d58b8aec80fae89c208ff8f5b9d18ee96ecf515
Olivia <69110329+livrbecca@users.noreply.github.com>
2021-10-18 17:08:09 +0100

    Add project files.

---
+++ BoxExample/Box.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YTCC1
+{
+    class Box // creating a data type that looks like a box
+    {
+        public double Length { get; set; }
+        // properties of classes should get capital letters
+        // get, allows you to retrieve the value. set, allows you to set a value
+        // short cut: prop then tab x2
+        // if you removed set you would get an error, when try tring to assign a value - would be read only / retrieve only
+        // if you only had set & not get: error wehn trying to Console.WriteLine it, you can set it but you cannot retrieve it
+        public double Breadth { get; set; }
+        public double Height { get; set; }
+
+
+        // inside of Box class: method to calculate volume
+
+
+        public double getVolume()
+        // doesnt need to take params because its part of the class that already contains the properties
+        {
+            return Length * Breadth * Height;
+        }
+
+        public double getArea()
+        // doesnt need to take params because its part of the class that already contains the properties
+        {
+            return Length * Breadth;
+        }
+    }
+}
---
+++ BoxExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace YTCC1
+{
+    class Program
+    // class: blueprint for a data-type. allows you to define your own DT
+    // which can be a combination of other base types (string, int, ect) and methods
+    {
+        static void Main(string[] args)
+        {
+            // to use Box datatype: we want a variable that represents Box
+            // create/initialize Object of class type Box
+            Box boxObject = new Box(); // box = new instance of Box, it is now an object,not a variable
+
+            Console.WriteLine("Enter a length: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a breadth: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Height: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+
+            // doing boxObject. shows the properties you have access to
+            boxObject.Length = n1; // SETTING the values (thanks to {get; set;})
+            boxObject.Breadth = n2;
+            boxObject.Height = n3;
+            double volume = boxObject.getVolume();
+            double area = boxObject.getArea();
+            // example of 'get' / getting: because you are viewing the values
+            Console.WriteLine($"Box dimensions are:  {boxObject.Length}, {boxObject.Breadth}, {boxObject.Height}\nthe volume is {volume}\nthe area is {area}");
+
+
+
+            // INITIALIZNG A NEW BOX OBJECT: can have multiple objects of the same data type long as names are diff
+            Box boxObj2 = new Box();
+
+
+            Console.WriteLine("Enter a length: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a breadth: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Height: ");
+            n3 = double.Parse(Console.ReadLine());
+
+
+            // doing boxObject. shows the properties you have access to
+            boxObj2.Length = n1; // SETTING the values (thanks to {get; set;})
+            boxObj2.Breadth = n2;
+            boxObj2.Height = n3;
+            //volume = boxObj2.getVolume();
+            //area = boxObj2.getArea();
+            // instead of declaring variables, call the methods in the formatted string
+
+            Console.WriteLine($"Second Box dimensions are:  {boxObj2.Length}, {boxObj2.Breadth}, {boxObj2.Height}\nthe volume is {boxObj2.getVolume()}\nthe area is {boxObj2.getVolume()}");
+
+
+        }
+    }
+
+}
